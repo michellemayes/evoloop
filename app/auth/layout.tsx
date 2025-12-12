fd4f55fba57006c5198f0b2efabcaa57d9781309
Michelle Mayes <michellep1994@gmail.com>
2025-12-12 01:46:30 -0600

    fix auth styling

---
+++ app/auth/layout.tsx
@@ -0,0 +1,6 @@
+// app/auth/layout.tsx
+import '@neondatabase/neon-js/ui/css';
+
+export default function AuthLayout({ children }) {
+  return <>{children}</>;
+}
--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -3,12 +3,16 @@
 import { NeonAuthUIProvider, UserButton } from '@neondatabase/neon-js/auth/react/ui';
 import { authClient } from '@/lib/auth-client';
 
+
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   return (
     <NeonAuthUIProvider
       authClient={authClient}
       redirectTo="/dashboard"
       emailOTP
+      social={{  
+        providers: ['google']  
+        }} 
     >
       {children}
     </NeonAuthUIProvider>
